0fa571ffc8984df704180ead8d69374e30784c64
Axl Wang <axlwang1976@gmail.com>
2019-07-08 13:58:56 +0800

    add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,17 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from '../styles/Header.module.scss';
 
 export default function Header() {
   return (
     <AppBar position="static">
-      <Toolbar>
+      <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography variant="h4">Realtime Chat</Typography>
+        <Button variant="outlined" color="inherit" className={styles.button}>
+          Login
+        </Button>
       </Toolbar>
     </AppBar>
   );
