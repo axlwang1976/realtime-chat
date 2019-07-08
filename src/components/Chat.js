7915478e211c31135c67960f9b67189be20ed844
Axl Wang <axlwang1976@gmail.com>
2019-07-08 12:00:02 +0800

    set local storage

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,7 +5,12 @@ import ChatForm from './ChatForm';
 
 export default class Chat extends Component {
   state = {
-    chats: [],
+    chats: JSON.parse(localStorage.getItem('chats')) || [],
+  };
+
+  syncLocalStorage = () => {
+    const { chats } = this.state;
+    localStorage.setItem('chats', JSON.stringify(chats));
   };
 
   addChat = msg => {
@@ -14,7 +19,7 @@ export default class Chat extends Component {
     const nowString = `${now.getFullYear()}/${now.getMonth() +
       1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
     const newChat = { msg, createdTime: nowString };
-    this.setState({ chats: [...chats, newChat] });
+    this.setState({ chats: [...chats, newChat] }, this.syncLocalStorage);
   };
 
   render() {
